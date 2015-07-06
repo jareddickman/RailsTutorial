fec3131528e8ab1b118ce3b70da095899719afa9
jareddickman <jdickman@stevens.edu>
2015-07-05 21:36:31 -0400

    Added micropost feature

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705225651) do
+ActiveRecord::Schema.define(version: 20150706010333) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.string   "text"
+    t.string   "user_id"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
