f879c62f3bfe6ee571e8aa5b5793ac9f4b7e694a
jareddickman <jdickman@stevens.edu>
2015-07-19 23:14:14 -0400

    Logging in the user upon signup

--- Microblogging/app/controllers/users_controller.rb
+++ Microblogging/app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
-def create
+
+  def new
+    @user = User.new
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucess] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
