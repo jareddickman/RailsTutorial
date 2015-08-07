5f0d6785523c6e23a6b972fbc53f0d7dd3a55f8b
Jared Dickman <jdickman@stevens.edu>
2015-08-06 21:24:06 -0400

    Finish user edit, update, index, and destroy actions

--- Microblogging/test/controllers/users_controller_test.rb
+++ Microblogging/test/controllers/users_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
-  def setup
-    @user = users(:michael)
+   def setup
+    @user       = users(:michael)
     @other_user = users(:archer)
   end
 
@@ -41,5 +41,20 @@ class UsersControllerTest < ActionController::TestCase
    test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
   end
 end
--- Microblogging/test/integration/users_index_test.rb
+++ Microblogging/test/integration/users_index_test.rb
@@ -3,16 +3,15 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
+ 
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
     get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-    end
+    assert_select 'a', text: 'delete', count: 0
   end
 end
